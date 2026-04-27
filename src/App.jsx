1e1a557d1ec8ecdcdcb6cc38777f9c50a8f30dfd
federicoacker <acker.federico@gmail.com>
2026-04-27 14:18:27 +0200

    Aggiunte funzioni base ai componenti che sto usando per ora, ora devo piano piano scriverle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container  from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
+import Main from './components/Main.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 function App() {
   return (
     <>
     <Container fluid="lg">
-      <Row></Row>
-      <Row></Row>
-      <Row></Row>
+      <Row>
+        <Col>
+          <Header/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Main/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Footer/>
+        </Col>
+      </Row>
     </Container>
     </>
   )
