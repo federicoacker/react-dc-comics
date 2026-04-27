cdc5d350f79c249390bb1e687ecc525575ec591b
federicoacker <acker.federico@gmail.com>
2026-04-27 22:58:12 +0200

    Aggiunto link ai link nel footer e aggiunto il font

--- src/components/FooterNavlink.jsx
+++ src/components/FooterNavlink.jsx
@@ -3,12 +3,14 @@
 function FooterNavlink({name, url}){
 
     return (
+    <a href="" className="d-flex column-gap-2 align-items-center footer-navbar-links">
     <figure className="d-flex column-gap-2 align-items-center">
         <img src={ url } className="footer-navbar-img-fluid"></img>
-        <figcaption className="text-white">
+        <figcaption>
             { name }
         </figcaption>
     </figure>
+    </a>
     );
 }
 
