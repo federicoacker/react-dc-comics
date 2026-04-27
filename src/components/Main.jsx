b0bbfb57e7c4c25eea34f15edf91f2986ceaefbb
federicoacker <acker.federico@gmail.com>
2026-04-27 14:39:22 +0200

    Aggiunto classi e un piccolo trucco per aggiungere un background che vada oltre la limitazione data dal container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,11 @@
+import Container from 'react-bootstrap/Container';
+
 function Main(){
     return(
         <>
+        <Container className = "main-container">
+            <h3 className= "text-white">---&gt; Content goes here &lt;---</h3>
+        </Container>
         </>
     )
 }
