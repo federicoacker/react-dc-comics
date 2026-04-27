9ba411ce8169402bf3368b46aea41018109944ea
federicoacker <acker.federico@gmail.com>
2026-04-27 15:08:36 +0200

    Fatto modifiche a FooterNavLink e iniziato a sperimentare con un modo per passargli dati quando la creo

--- src/components/FooterNavbar.jsx
+++ src/components/FooterNavbar.jsx
@@ -3,18 +3,37 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import FooterNavlink from './FooterNavlink';
 
-const links = ['Digital Comics', 'DC Merchandise', 'Subscription', 'Comic Shop Locator', 'DC Power Visa'];
+const links = [
+    {
+        name:'Digital Comics', 
+        url:'/img/buy-comics-digital-comics.png'
+    },
+    {
+        name:'DC Merchandise',
+        url:'/img/buy-comics-merchandise.png'
+    },
+    {
+        name:'Subscription',
+        url:'/img/buy-comics-subscriptions.png'
+    },
+    {
+        name:'Comic Shop Locator',
+        url: '/img/buy-comics-shop-locator.png'
+    }, 
+    {
+        name:'DC Power Visa',
+        url:'/img/buy-dc-power-visa.svg'
+    }
+];
 
 function FooterNavbar(){
     return(
         <>
         <Navbar expand="md" className="footer-navbar">
             <Container>
-                <Navbar.Toggle aria-controls="footer-navbar-nav"/>
-                <Navbar.Collapse id="footer-navbar-nav" className="justify-content-between">
-                    <Nav className="footer-navbar-links">
-                    </Nav>
-                </Navbar.Collapse>
+                <Nav className="footer-navbar-links">
+                    <FooterNavlink name={ links[0].name } url= { links[0].url }></FooterNavlink>
+                </Nav>
             </Container>
         </Navbar>
         </>
--- src/components/FooterNavlink.jsx
+++ src/components/FooterNavlink.jsx
@@ -1,11 +1,15 @@
-function FooterNavlink(){
-    return(
-        <>
-        <figure>
-            
-        </figure>
-        </>
-    )
+
+
+function FooterNavlink({name, url}){
+
+    return (
+    <figure className="d-flex">
+        <img src={ url } className="img-fluid"></img>
+        <figcaption>
+            { name }
+        </figcaption>
+    </figure>
+    );
 }
 
 export default FooterNavlink
