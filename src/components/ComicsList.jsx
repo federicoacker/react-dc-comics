99a77b21aad548269eb63704bc1c7b7dd5eb422b
federicoacker <acker.federico@gmail.com>
2026-04-28 18:01:38 +0200

    Aggiunto la ComicsList, usato delle classi per dargli object fit e object-position

---
+++ src/components/ComicsList.jsx
@@ -0,0 +1,22 @@
+import comics from '../utility/comics.js';
+import Col from 'react-bootstrap/Col';
+
+function ComicsList(){
+    return (
+    <>
+    {comics.map(comic =>{
+        return (<>
+        <Col xs={12} sm = {12} md = {4} lg ={2} className="comic">
+            <figure className="comic-thumb text-white">
+                <img src = { comic.thumb } className="img-fluid" alt={comic.title}></img>
+            </figure>
+            <h5 className="text-white">{ comic.series} </h5>
+        </Col>
+        </>
+        );
+    })}
+    </>
+    );
+}
+
+export default ComicsList;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
-
+import Row from "react-bootstrap/Row";
+import ComicsList from "./ComicsList";
 
 function Main(){
     return(
         <>
-        <h3 className= "text-white">---&gt; Content goes here &lt;---</h3>
+        <Row className="g-5 justify-content-center row-gap-5 comics-list">
+            <ComicsList/>
+        </Row>
+        <div className="pt-5">
+            <button className="btn btn-primary rounded-0 text-center d-block mx-auto px-5">Load More</button>
+        </div>
         </>
     )
 }
